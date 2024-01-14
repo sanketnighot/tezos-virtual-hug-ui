5b8b9365e5eaf6ea2bc436b7991ca59d0a7d3cfe
uzupis <84919649+uzupis@users.noreply.github.com>
2024-01-14 11:31:57 +0100

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -2,5 +2,5 @@ export const NETWORK = 'ghostnet';
 export const RPC = 'https://ghostnet.ecadinfra.com';
 export const EXPLORER = 'https://ghostnet.tzkt.io';
 export const NFT_CONTRACT_ADDRESS = 'KT1KWncvw3rj539n3ebqGW3XtWN76MU7X2xE';
-export const HUX_CONTRACT_ADDRESS = 'KT1SCVXzraYHqoENcg8mkF3fTAr49VzuMFLf';
+export const HUX_CONTRACT_ADDRESS = 'KT1MgzSQtKuzce3GQoo776K68EU7mpcPH8Fk';
 export const API = "https://api.ghostnet.tzkt.io";
