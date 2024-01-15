126b4ce3e702facbd25e029c0ebdae5c20cffd1e
sanketnighot <sanketnighot25@gmail.com>
2024-01-15 15:25:10 +0530

    Removed API Keys

--- utils/config.js
+++ utils/config.js
@@ -4,5 +4,3 @@ export const EXPLORER = 'https://ghostnet.tzkt.io';
 export const NFT_CONTRACT_ADDRESS = 'KT1LifA9cJ91fbPR9PRjajXzkcivSgA9LFDV';
 export const HUX_CONTRACT_ADDRESS = 'KT1MgzSQtKuzce3GQoo776K68EU7mpcPH8Fk';
 export const API = "https://api.ghostnet.tzkt.io";
-export const API_Key = "3c3005349d8ec0dce3e0"
-export const API_Secret = "adbac881c918ee17d923011a8d39a29a3cc278e5bf7ca4fa547891d68b2617c"
