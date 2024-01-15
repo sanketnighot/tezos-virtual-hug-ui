a105dd97b71546b5a4ecef61a85578e673460595
sanketnighot <sanketnighot25@gmail.com>
2024-01-15 15:19:56 +0530

    Updated config file

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,8 @@
 export const NETWORK = 'ghostnet';
 export const RPC = 'https://ghostnet.ecadinfra.com';
 export const EXPLORER = 'https://ghostnet.tzkt.io';
-export const NFT_CONTRACT_ADDRESS = 'KT1KWncvw3rj539n3ebqGW3XtWN76MU7X2xE';
-export const HUX_CONTRACT_ADDRESS = 'KT1SCVXzraYHqoENcg8mkF3fTAr49VzuMFLf';
+export const NFT_CONTRACT_ADDRESS = 'KT1LifA9cJ91fbPR9PRjajXzkcivSgA9LFDV';
+export const HUX_CONTRACT_ADDRESS = 'KT1MgzSQtKuzce3GQoo776K68EU7mpcPH8Fk';
 export const API = "https://api.ghostnet.tzkt.io";
+export const API_Key = "3c3005349d8ec0dce3e0"
+export const API_Secret = "adbac881c918ee17d923011a8d39a29a3cc278e5bf7ca4fa547891d68b2617c"
